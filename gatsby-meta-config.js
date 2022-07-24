f4cfa6aec8dee2aa1513ea122d055e70779758f9
Jwhyee <kawnsdud@naver.com>
2022-07-24 18:04:37 +0900

    내용 설정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  description: `Jwhy Repository`,
+  author: `[Jwhy]`,
+  introduction: `hello world`,
+  siteUrl: `https://jwhyee.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
@@ -16,13 +16,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'Jwhyee/Jwhyee.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'jwhy',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
