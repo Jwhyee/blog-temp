23d408a670a20f1d6240ff8b81b60d6c441630a4
Jwhyee <Jwhyee@users.noreply.github.com>
2022-08-20 05:04:00 +0000

    Automated deployment: Sat Aug 20 05:04:00 UTC 2022 4911c97c44e1cc71b76e7769b7a47f3e398b6b5d

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a7e16ac1eb0944b324d4.js"
+    "url": "webpack-runtime-800203f04714e93e3b8c.js"
   },
   {
     "url": "styles.6e4fdc2f3b1b83a07768.css"
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-727eca7a674e10b21a9f.js"
   },
   {
-    "url": "app-93ecd53fb4d37315cfd3.js"
+    "url": "app-77e3ba791b4343802b54.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-38fe08ef875e5952d5f0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bf47ac7a98d71b2a4615db13c31238c4"
+    "revision": "c668c7625b13e44bcbf08d99dc21c4f2"
   },
   {
     "url": "manifest.webmanifest",
@@ -73,7 +73,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-93ecd53fb4d37315cfd3.js`))) {
+  if (!resources || !(await caches.match(`/app-77e3ba791b4343802b54.js`))) {
     return await fetch(event.request)
   }
 
